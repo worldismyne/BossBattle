f82553156bea192e5bd30033b631aa1d565a9f46
worldismyne <50659952+worldismyne@users.noreply.github.com>
2020-03-25 05:07:13 -0700

    Here it is!
    Second attempt to transfer everything over neatly

---
+++ BossBattleDempo/Assets/Scripts/Battle System.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class Battlesystem : MonoBehaviour
+
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    Unit Nagen;
+    Unit Boss;
+
+    public Text dialogueText;
+
+    public BattleState state;
+
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+      
+        Nagen = playerPrefab.GetComponent<Unit>();
+
+        Boss = enemyPrefab.GetComponent<Unit>();
+
+        dialogueText.text = "Oh no" + Boss.unitName + "has declared war.";
+    }
+}
---
+++ BossBattleDempo/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public Dropdown Dropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    { PopulateList(); }
+
+    void PopulateList()
+    {
+        resolutions = Screen.resolutions;
+        Dropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        Dropdown.AddOptions(options);
+        Dropdown.value = currentResolutionIndex;
+        Dropdown.RefreshShownValue();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+    
+    public void SetQuality (int qualityIndex)
+    { QualitySettings.SetQualityLevel(qualityIndex); }
+
+    public void SetFullscreen (bool isFullscreen)
+    { Screen.fullScreen = isFullscreen; }
+}
